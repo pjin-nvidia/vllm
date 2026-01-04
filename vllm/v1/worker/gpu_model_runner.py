75800a9b9890b5f0dd89db69ee6d4538d1ec88ec
Tomer Natan <tbarnatan@ipp1-1428.ipp1a1.colossus.nvidia.com>
2026-01-04 00:44:13 -0800

    change max model len
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -631,7 +631,7 @@ class GPUModelRunner(
             num_fused_shared_experts=num_fused_shared_experts,
             num_batched_tokens=self.scheduler_config.max_num_batched_tokens,
             max_running_requests=max_running_requests,
-            max_model_len=self.scheduler_config.max_model_len,
+            max_model_len=self.max_model_len,
             device=self.device,
             rank=rank,
             world_size=world_size,
